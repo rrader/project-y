84a4a6a52c94f9a219dac46f85e4398e9bfc8506
Sergei Nanovskyi <sunmille@gmail.com>
2015-10-25 14:58:28 +0200

    frontend automation

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,74 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var uglify = require('gulp-uglify');
-var react = require('gulp-react');
-var htmlreplace = require('gulp-html-replace');
 var exec = require('child_process').exec
 
 var path = {
-  FILES: ['ui/**/*.html', 'ui/**/*.css', 'ui/**/*.js'],
-  JSX: ['ui/js/*.jsx', 'ui/js/**/*.jsx'],
-  MINIFIED_OUT: 'build.min.js',
-  DEST_JS: 'ui-dist/js',
-  DEST: 'ui-dist'
+  static: ['ui/**/*.html'],
+  jsMain: 'ui/js/app.js',
+  jsFiles: ['ui/js/*.js', 'ui/js/**/*.js'],
+  cssFiles: ['ui/css/*.css', 'ui/css/**/*.css'],
+  jsDev: 'ui/js',
+  cssDev: 'ui/css',
+  jsProd: 'ui-dist/js',
+  cssProd: 'ui-dist/css',
+  prod: 'ui-dist'
 };
 
-gulp.task('transform', function(){
-  gulp.src(path.JSX)
-    .pipe(react())
-    .pipe(gulp.dest(path.DEST_JS));
+function concatCSS (output) {
+  return gulp.src(path.cssFiles)
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest(output));
+}
+
+function browserifyJS (output) {
+  var b = browserify({
+    entries: path.jsMain,
+    transform: [babelify]
+  });
+
+  return b.bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest(output));
+}
+
+gulp.task('bundle-js', function () {
+  browserifyJS(path.jsDev);
 });
 
-gulp.task('copy', function(){
-  gulp.src(path.FILES)
-    .pipe(gulp.dest(path.DEST));
+gulp.task('bundle-js-prod', function () {
+  browserifyJS(path.jsProd);
 });
 
-gulp.task('build', ['transform', 'copy']);
+gulp.task('bundle-css', function () {
+  concatCSS(path.cssDev);
+});
 
-gulp.task('watch', ['build'], function () {
-    gulp.watch(path.JSX, ['transform']);
-    gulp.watch(path.FILES, ['copy']);
+gulp.task('bundle-css-prod', function () {
+  concatCSS(path.cssProd);
+});
+
+gulp.task('copy-static', function (){
+  gulp.src(path.static)
+    .pipe(gulp.dest(path.prod));
+});
+
+gulp.task('build', ['bundle-js-prod', 'bundle-css-prod', 'copy-static']);
+
+gulp.task('watch-css', function () {
+  gulp.watch(path.cssFiles, ['bundle-css']);
+});
+
+gulp.task('watch', function () {
+  gulp.watch(path.cssFiles, ['bundle-css-prod']);
+  gulp.watch(path.jsFiles, ['bundle-js-prod']);
+  gulp.watch(path.static, ['copy-static']);
 });
 
 // Python tasks
@@ -48,4 +88,3 @@ gulp.task('serve', function() {
 
 gulp.task('default', ['watch', 'serve']);
 gulp.task('production', ['build']);
-
