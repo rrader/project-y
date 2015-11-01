75ccbebd012454738310a0de8cd668c0131757c5
Sergei Nanovskyi <sunmille@gmail.com>
2015-11-01 13:28:28 +0200

    switch to scss: base + header

--- ui/js/app.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import PageComponent from './components/PageComponent';
-
-ReactDOM.render(<PageComponent/>, document.getElementById('container'));
